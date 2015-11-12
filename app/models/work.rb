baec45571d6fdfae35c3a8dc109bd8bbfd2a9986
qiliao17 <qiliao17@gmail.com>
2015-11-12 21:55:21 +0000

    fixed formatting of search bar

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,6 @@
 class Work < ActiveRecord::Base
     def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
-    where("artist like ?", "%#{query}%") 
+    where("artwork like ?", "%#{query}%") 
     end
 end
