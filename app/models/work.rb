a1f431d8ebf6da287b5248222402ebc3ff56aa3d
qiliao17 <qiliao17@gmail.com>
2016-03-04 17:14:52 +0000

    updated search

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,12 @@
 class Work < ActiveRecord::Base
     def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
-    where("artwork like ?", "%#{query}%") 
+    where("artist  like ?", "%#{query}%") 
+    where("artwork  like ?", "%#{query}%") 
+    where("medium  like ?", "%#{query}%") 
+    where("momanumber  like ?", "%#{query}%") 
+    where("classification  like ?", "%#{query}%") 
+    where("department  like ?", "%#{query}%") 
+    where("artist  like ?", "%#{query}%") 
     end
 end
